01c0631d407c273a96aae66e3b0450774540dfa4
Vilmar.Ricken <vilmar.ricken@gmail.com>
2015-03-20 17:20:08 -0300

    zueira

--- server/ZueiraServer/src/main/java/com/master/zueira/server/ZueiraDiscover.java
+++ server/ZueiraServer/src/main/java/com/master/zueira/server/ZueiraDiscover.java
@@ -5,48 +5,52 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class DiscoveryThread implements Runnable {
+public class ZueiraDiscover implements Runnable {
 
-	private static class DiscoveryThreadHolder {
-		private static final DiscoveryThread INSTANCE = new DiscoveryThread();
-	}
+	private static final String ACCEPT_MESSAGE = "Zueira connected in: ";
 
-	public static DiscoveryThread getInstance() {
-		return DiscoveryThreadHolder.INSTANCE;
+	private static ZueiraDiscover INSTANCE;
+
+	public static ZueiraDiscover getInstance() {
+		if (INSTANCE == null) {
+			synchronized (ACCEPT_MESSAGE) {
+				if (INSTANCE == null) {
+					INSTANCE = new ZueiraDiscover();
+					new Thread(INSTANCE).start();
+				}
+			}
+		}
+		return INSTANCE;
 	}
 
 	public static void main(final String[] args) {
-		new Thread(getInstance()).start();
+		getInstance();
 	}
 
 	private DatagramSocket socket;
 
+	private ZueiraDiscover() {
+	}
+
 	@Override
 	public void run() {
 		try {
-			// Keep a socket open to listen to all the UDP trafic that is
-			// destined for this port
 			this.socket = new DatagramSocket(8888, InetAddress.getByName("0.0.0.0"));
 			this.socket.setBroadcast(true);
-
 			while (true) {
 				System.out.println(this.getClass().getName() + ">>>Ready to receive broadcast packets!");
 
-				// Receive a packet
 				final byte[] recvBuf = new byte[15000];
 				final DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
 				this.socket.receive(packet);
 
-				// Packet received
 				System.out.println(this.getClass().getName() + ">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
 				System.out.println(this.getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
 
-				// See if the packet holds the right command (message)
 				final String message = new String(packet.getData()).trim();
-				if (message.equals("DISCOVER_FUIFSERVER_REQUEST")) {
+				if (message.startsWith(ACCEPT_MESSAGE)) {
 					final byte[] sendData = "DISCOVER_FUIFSERVER_RESPONSE".getBytes();
 
-					// Send a response
 					final DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
 					this.socket.send(sendPacket);
 
--- server/ZueiraServer/src/main/java/com/master/zueira/server/ZueiraServer.java
+++
@@ -1,9 +0,0 @@
-package com.master.zueira.server;
-
-public class ZueiraServer {
-
-	public ZueiraServer() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
